e91cf9eb9a55e9d580c48c65447f92e79a4becad
ddiu8081 <ddiu8081@gmail.com>
2023-10-26 19:03:00 +0800

    chore: update pwa settings

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,18 @@
 import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
 import { clientsClaim } from 'workbox-core'
+import { NavigationRoute, registerRoute } from 'workbox-routing'
 
 self.skipWaiting()
 clientsClaim()
 
 cleanupOutdatedCaches()
 precacheAndRoute(self.__WB_MANIFEST)
+
+registerRoute(new NavigationRoute(
+  createHandlerBoundToURL('index.html'),
+  { denylist: [
+    /^\/api/,
+    /^\/stats/,
+    /^\/setlist/,
+  ] },
+))
